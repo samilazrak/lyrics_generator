6e9c0331cc52be8e6586f1e09395db2596c41069
Sami Lazrak <sami.lazrak@etu.unice.fr>
2019-05-26 13:39:54 +0200

    rihanna with short lyrics

--- python/larger_lstm/generating_text.py
+++ python/larger_lstm/generating_text.py
@@ -8,7 +8,7 @@ from keras.layers import LSTM
 from keras.callbacks import ModelCheckpoint
 from keras.utils import np_utils
 # load ascii text and covert to lowercase
-filename = "../../singer_lyrics/rihanna.txt"
+filename = "../../singer_lyrics/rihanna_long.txt"
 raw_text = open(filename).read()
 raw_text = raw_text.lower()
 # create mapping of unique chars to integers, and a reverse mapping
@@ -45,7 +45,7 @@ model.add(LSTM(256))
 model.add(Dropout(0.2))
 model.add(Dense(y.shape[1], activation='softmax'))
 # load the network weights
-filename = "weights-improvement-01-2.5065-bigger.hdf5"
+filename = "weights-improvement-73-1.2218-bigger.hdf5"
 model.load_weights(filename)
 model.compile(loss='categorical_crossentropy', optimizer='adam')
 # pick a random seed
